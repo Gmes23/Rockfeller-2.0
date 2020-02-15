fbc3013de30be470912deb5f927478737bb92382
gm_desktop <gmesda23@gmail.com>
2020-02-14 22:59:27 -0500

    refactored codebase

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -50,9 +50,9 @@ const appReducer = (state = initialState, action) =>
         break;
 
       case SET_USER:
-       draft.user = action.user;
-       draft.isAuth = action.isAuth;
-      break;
+        draft.user = action.user;
+        draft.isAuth = action.isAuth;
+        break;
 
       case CLEAR_USER:
         return {
